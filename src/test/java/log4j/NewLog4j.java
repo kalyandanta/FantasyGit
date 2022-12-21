7b6677dfb414f769f6ce3fad1fc691ed4b68b7a0
kalyandanta <121169541+kalyandanta@users.noreply.github.com>
2022-12-21 17:47:38 +0530

    NewLog4j class updates

--- src/test/java/log4j/NewLog4j.java
+++ src/test/java/log4j/NewLog4j.java
@@ -2,11 +2,13 @@ package log4j;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 
 public class NewLog4j {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void log4jtutorial() {
   }
   @BeforeTest
   public void beforeTest() {
